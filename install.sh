95017975cb6fade0273cbb056b4ae57b47970c5a
rvnminers-A-and-N <86896075+rvnminers-A-and-N@users.noreply.github.com>
2021-11-14 19:33:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This is the Pool install script.
-echo "JAMPS Ravencoin Pool install script."
+echo "Crypticwizardry Ravencoin Pool install script."
 echo "Please do NOT run as root, run as the pool user!"
 
 echo "Installing... Please wait!"
